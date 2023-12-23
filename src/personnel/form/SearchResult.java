4d62d1ce3a2e4b7449afed821b5e70da0870b6b9
Risto Libera <qofgxnm7@gmail.com>
2023-12-23 23:52:53 +0900

    Update font

--- src/personnel/form/SearchResult.java
+++ src/personnel/form/SearchResult.java
@@ -5,7 +5,7 @@ package personnel.form;
  */
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import java.awt.BorderLayout;
+import javax.swing.table.JTableHeader;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
@@ -18,17 +18,22 @@ public class SearchResult extends JFrame{
         setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
         // Left input area
         lb_name = new JLabel("Name");
-        lb_name.setFont(new Font("Arial", Font.PLAIN, 25));
+        lb_name.setFont(new Font("Arial", Font.PLAIN, 20));
         tf_name = new JTextField(30);
-        tf_name.setFont(new Font("Arial", Font.PLAIN, 20));
+        tf_name.setFont(new Font("Arial", Font.PLAIN, 15));
         // panel_title for responsive layout
-        JPanel panel_down = new JPanel();
-        panel_down.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 10));
-        panel_down.setBackground(Color.GRAY);
+        JPanel panel_search = new JPanel();
+        panel_search.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 10));
+        panel_search.setBackground(Color.GRAY);
 
         // Right result area
         DefaultTableModel tableModel = new DefaultTableModel();
         JTable table = new JTable(tableModel);
+        table.setFont(new Font("Times New Roman", Font.BOLD, 25));
+        table.setRowHeight(30);
+        JTableHeader tableHeader = table.getTableHeader();
+        tableHeader.setFont(new Font("Arial", Font.PLAIN, 30));
+
         tableModel.setColumnIdentifiers(new Object[] { "CSS", "HTML5", "JavaScript" });
         tableModel.insertRow(0, new Object[] { "HTML5", "test" });
         tableModel.insertRow(0, new Object[] { "JavaScript" });
@@ -43,15 +48,14 @@ public class SearchResult extends JFrame{
         tableModel.insertRow(tableModel.getRowCount(), new Object[] { "ExpressJS" });
 
         // Add to frame
-        panel_down.add(lb_name);
-        panel_down.add(tf_name);
+        panel_search.add(lb_name);
+        panel_search.add(tf_name);
         add(new JScrollPane(table));
-        add(panel_down);
+        add(panel_search);
         // Initial settings
         setSize(800, 400);
         setLocationRelativeTo(null);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
     }
 }
